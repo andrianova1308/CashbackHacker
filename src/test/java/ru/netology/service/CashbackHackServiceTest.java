d4d9ecf23a2571552b52fce64f6bbcc3fcbcff34
Анастасия Андрианова <samsikovaa@mail.ru>
2022-09-06 15:13:48 +0300

    TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,20 +1,45 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
 
 public class CashbackHackServiceTest {
 
 
     @Test
 
-    public void calculateCashback() {
+    public void calculateCashbackBeforeBoundary() {
 
         CashbackHackService cashback = new CashbackHackService();
         int amount = 900;
 
         int actual = cashback.remain(amount);
         int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void calculateCashbackAfterBoundary() {
+
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1700;
+
+        int actual = cashback.remain(amount);
+        int expected = 300;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+
+    public void calculateCashbackEqualsBoundary() {
+
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashback.remain(amount);
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
